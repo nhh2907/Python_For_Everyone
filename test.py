2de96d7e07ea584e0830998640c8071779afe197
Nogari <78652856+nhh2907@users.noreply.github.com>
2023-03-15 00:04:20 +0900

    delete .DS_stor

--- test.py
+++
@@ -1,6 +0,0 @@
-test_list = [str(x) for x in range(1, 1000)]
-test_sort = sorted(test_list)
-test_sort2 = sorted(test_list, key=lambda x: int(x))
-
-for i in zip(test_sort, test_sort2):
-    print(i)
